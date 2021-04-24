af83470ac4de7a5be0e2785f5cdc146ab3e3d8ff
Trevor McGlaflin <trevor.mcglaflin@uvm.edu>
2021-04-24 12:50:39 -0400

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/M4OEP_Graphics_tmcglafl.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/M4OEP_Graphics_tmcglafl.dir/main.cpp.obj"
+  "M4OEP_Graphics_tmcglafl.exe"
+  "M4OEP_Graphics_tmcglafl.exe.manifest"
+  "M4OEP_Graphics_tmcglafl.pdb"
+  "libM4OEP_Graphics_tmcglafl.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/M4OEP_Graphics_tmcglafl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/M4OEP_Graphics_tmcglafl.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for M4OEP_Graphics_tmcglafl.
+# This may be replaced when dependencies are built.
